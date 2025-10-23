00b6e7bab36e1da3a97a9692f7ea1de31e17d75f
mariaqueiroz00 <mariaeduardaqueirozmoreira@gmail.com>
2025-10-23 20:08:20 -0300

    Add files via upload

---
+++ Router.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router";
+import IndexPage from "./pages/IndexPage";
+import PainelPage from "./pages/PainelPage";
+import PerfilPage from "./pages/profile/PerfilPage";
+import TelaLogin from "./pages/login/Login";
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<IndexPage />} />
+      <Route path="/painel" element={<PainelPage />} />
+      <Route path="/perfil" element={<PerfilPage />} />
+      <Route path="/login" element={<TelaLogin />} />
+      <Route path="/duda" element={<TelaLogin />} />
+
+    </Routes>
+  );
+}
