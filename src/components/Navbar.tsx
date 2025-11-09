7d58634003cdfdeeede58174f4f690b580eaf8b7
Otavio Bigogno <otaviobigogno37@gmail.com>
2025-11-09 17:52:16 -0300

    Icone corrigido

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../contexts/UserContext";
 import { useTheme } from "../contexts/ThemeContext";
+import { MoonStar, Sun } from "lucide-react";
 
 export default function Navbar() {
   const nav = useNavigate();
@@ -24,12 +25,8 @@ export default function Navbar() {
         {/* SWITCH DE TEMA */}
         <label className="swap swap-rotate cursor-pointer">
           <input type="checkbox" onChange={toggleTheme} checked={theme === "dark"} />
-          <svg className="swap-on fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M5.64 17l-.71.71a1 1 0 01-1.42 0l-.71-.71a1 1 0 010-1.42l.71-.71M18.36 7l.71-.71a1 1 0 000-1.42l-.71-.71a1 1 0 00-1.42 0l-.71.71M12 3v-1M12 22v-1M4 12H3M22 12h-1M6.36 6.36l-.71-.71M18.36 18.36l-.71-.71"></path>
-          </svg>
-          <svg className="swap-off fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M21.64 13a1 1 0 00-1.05-.14 8.05 8.05 0 01-3.37.73 8.15 8.15 0 01-8.14-8.14A8 8 0 019.86 2.3a1 1 0 00-.61-1.48A10.14 10.14 0 1022 14.31a1 1 0 00-.36-1.31z"></path>
-          </svg>
+          <Sun className="swap-off h-8 w-8" />
+          <MoonStar className="swap-on h-8 w-8 " />
         </label>
 
         {/* AVATAR + MENU */}
