e3b6d867c4a64992d62244318e79a505bfe61b4d
Ricardo da Rocha <ricardodarocha@outlook.com>
2025-11-21 12:46:17 -0300

    ajuste no titulo do sistema

--- src/layout/home/start/index.tsx
+++ src/layout/home/start/index.tsx
@@ -4,22 +4,33 @@ import styles from "./styles.module.css";
 
 export function Start_Home_Layout() {
     return (
-        <div className={styles.start} style={{ backgroundImage: "url(/home/start/fundo.png)" }}>
-            <Container_Layout className={styles.container}>
-                <div className={styles.collumn}>
-                    <h1 id={styles.title}>Nome do criador de <span>currículo</span></h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</p>
+      <div
+        className={styles.start}
+        style={{ backgroundImage: "url(/home/start/fundo.png)" }}
+      >
+        <Container_Layout className={styles.container}>
+          <div className={styles.collumn}>
+            <h1 id={styles.title}>
+              <span>ocurriculo.online</span>
+            </h1>
+            <p>
+              Um framework versátil, simples e seguro, permitindo a criação de
+              currículos de forma estruturada.
+            </p>
 
-                    <button onClick={() => {
-                        window.location.href = "/inicio"
-                    }}>Começar</button>
-                </div>
+            <button
+              onClick={() => {
+                window.location.href = "/inicio";
+              }}
+            >
+              Começar
+            </button>
+          </div>
 
-                <div className={styles.collumn}>
-                    <img src="/home/img1.png" alt="" />
-                </div>
-
-            </Container_Layout>
-        </div>
-    )
+          <div className={styles.collumn}>
+            <img src="/home/img1.png" alt="" />
+          </div>
+        </Container_Layout>
+      </div>
+    );
 }
